c6964729f444e007265629a3e1417937708a596e
Ahmed Zarir Siddique <zarir.ahmeds@gmail.com>
2023-04-27 19:56:05 +0600

    version 2
    added login and registration page

--- daycare/app/Providers/RouteServiceProvider.php
+++ daycare/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -38,3 +38,4 @@ class RouteServiceProvider extends ServiceProvider
         });
     }
 }
+
